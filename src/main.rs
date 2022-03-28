e0d97627c9ac41435959cf9b0d0e0d3f059f83fa
Quinn Painter <quinn@geekymonkey.com>
2022-03-28 22:06:20 +0100

    load file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let rom_file = fs::read(&args[1]).expect("Unable to open ROM file");
+    println!("{:?}", args);
 }
